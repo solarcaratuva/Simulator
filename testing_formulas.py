10d7f9c20fb62d3f7c40ff0a0b413f9c9da1135c
Tanishk Govil <tanishkgovil@gmail.com>
2025-10-26 14:30:44 -0400

    added power produced formula

--- testing_formulas.py
+++ testing_formulas.py
@@ -3,7 +3,9 @@ import numpy as np
 
 # define constants:
 g = 9.81 # m/s^2, acceleration due to gravity
-mu = float('inf') # efficiency coefficient of the regenerative braking system
+regen_efficiency = float('inf') # efficiency coefficient of the regenerative braking system
+electrical_efficiency = float('inf') # efficiency coefficient of the electrical system
+solar_panel_efficiency = float('inf') # efficiency coefficient of the solar panel
 wheels = 4 # number of wheels on the vehicle
 mass = float('inf') # kg, mass of the vehicle
 battery_capacity = float('inf') # Wh, capacity of the vehicle's battery
@@ -21,7 +23,7 @@ def calculate_regenerative_energy(v_initial: float) -> float:
     Returns:
         float: Energy recovered in Wh.
     """
-    return (0.5 * mass * v_initial ** 2) * mu / 3600
+    return (0.5 * mass * v_initial ** 2) * regen_efficiency / 3600
 
 
 def calculate_solar_charge(global_horizontal_coefficient: float) -> float:
@@ -34,7 +36,7 @@ def calculate_solar_charge(global_horizontal_coefficient: float) -> float:
     Returns:
         float: Charge from solar panels in W.
     """
-    return solar_panel_area * global_horizontal_coefficient * mu 
+    return solar_panel_area * global_horizontal_coefficient * solar_panel_efficiency
 
 def power_consumption(e_curr, e_in, e_out): 
     """ Calculates the powerConsumption of the solar car by the minute minute
@@ -47,3 +49,19 @@ def power_consumption(e_curr, e_in, e_out):
     """
     e_total = e_curr + e_in/60 + e_out/60
     return e_total
+
+def power_produced(F_d: float, F_r: float, F_gr: float, v: float) -> float:
+    """
+    Calculates the power produced by the solar car
+
+    Args:
+        F_d (float): Drag force
+        F_r (float): Rolling resistance force
+        F_gr (float): Gradient Resistance Force
+        v (float): Velocity of the car
+
+    Returns:
+        float: Power produced by the solar car, in Watts
+    """
+    F_net = F_d + F_r + F_gr
+    return F_net * v * electrical_efficiency
